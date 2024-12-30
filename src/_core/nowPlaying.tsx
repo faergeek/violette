f620e7b96f03ccd1546f3e4e6f312d98df3be56a
Sergei Slipchenko <faergeek@gmail.com>
2024-12-30 12:53:40 +0400

    PlayerPosition -> PlaybackPosition

--- src/_core/nowPlaying.tsx
+++ src/_core/nowPlaying.tsx
@@ -25,7 +25,7 @@ import { useStoreMutate, useStoreState } from '../store/react';
 import { cn } from './cn';
 import { CoverArt } from './coverArt';
 import { IconButton } from './iconButton';
-import { PlayerPosition } from './playerPosition';
+import { PlaybackPosition } from './playbackPosition';
 import { Slider } from './slider';
 import { StarredIcon } from './starredIcon';
 
@@ -202,7 +202,7 @@ export function NowPlaying({
 
   return (
     <>
-      <PlayerPosition />
+      <PlaybackPosition />
 
       <div className="flex items-center gap-4 px-4 py-2">
         <IconButton
